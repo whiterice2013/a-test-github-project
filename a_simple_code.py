e950885a23a088383df19cc6a83b22463e2bf5c1
whiterice2013 <haohsiang2014@gmail.com>
2025-03-24 18:46:41 +0800

    Refactor a_simple_code.py to loop through print statements

--- a_simple_code.py
+++ a_simple_code.py
@@ -1,4 +1,5 @@
-print("nah chill ey?")
-print("I'm just a simple code")
-print("nothing to worry about")
-print("I'm just a simple code")
+for i in range(10):
+    print("nah chill ey?")
+    print("I'm just a simple code")
+    print("nothing to worry about")
+    print("I'm just a simple code")
